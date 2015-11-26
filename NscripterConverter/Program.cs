7375c04ee3da43b8c7a9b31b41fb79ce2dec268d
Rob <web@cultureselect.com>
2015-11-25 23:20:15 -0500

    Implementing a few more commands, fixing other ones
    Wave, Waveloop, Wavestop, bgm, stop. Some fixes to existing commands

--- NscripterConverter/Program.cs
+++ NscripterConverter/Program.cs
@@ -50,7 +50,7 @@ namespace NscripterConverter
                     else if(data[0].Contains("$") || data[0].Contains("#")) //annoying things
                         fname = data[0];
                     else
-                        fname = data[0].Substring(data[0].LastIndexOf("/"));
+                        fname = data[0].Substring(data[0].LastIndexOf("/") + 1);
 
                     String type;
                     if (data[0].IndexOf("event", StringComparison.OrdinalIgnoreCase) >= 0)
@@ -64,15 +64,122 @@ namespace NscripterConverter
                     bg.Label = fname.Split('.')[0];
 
                     curr.AddTo(Label.LabelTypes.Textures, bg);
+
+
+                    //Now do the command for it
+                    Command bgc = new Command();
+                    bgc.Comm = "Bg";
+                    bgc.Arg[0] = bg.Label;
+
+                    curr.AddTo(Label.LabelTypes.Commands, bgc);
+
                 }
-                else if (line.StartsWith("wave", StringComparison.OrdinalIgnoreCase) || line.StartsWith("bgm", StringComparison.OrdinalIgnoreCase))
+                else if (line.StartsWith("wave", StringComparison.OrdinalIgnoreCase) && !line.StartsWith("waveloop", StringComparison.OrdinalIgnoreCase) && !line.StartsWith("wavestop", StringComparison.OrdinalIgnoreCase))
                 {
                     //Handle a new Sound
+                    Sound sd = new Sound();
+                    String tline = line;
+                    if (line.StartsWith("wave\"", StringComparison.OrdinalIgnoreCase))
+                    {
+                        tline = line.Substring(0, 4) + " \"" + line.Substring(5);
+                    }
 
+                    String[] data = tline.Split(' ').Select(d => d.Trim(new Char[] { ' ', '"' })).ToArray();
+
+                    String fname = data[1].Substring(data[1].LastIndexOf("/") + 1);
+                    String Label = fname.Split('.')[0];
+
+                    sd.FileName = fname;
+                    sd.Label = Label;
+                    sd.Type = "Se";
+                    sd.Title = Label;
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Sounds, sd);
+
+                    //Now do the command for it
+                    Command sdc = new Command();
+                    sdc.Comm = "Se";
+                    sdc.Arg[0] = sd.Label;
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, sdc);
+                }
+                else if (line.StartsWith("waveloop", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Handle a looping wave (We'll use ambience for this!)
+                    Sound sd = new Sound();
+                    String[] data = line.Split(' ').Select(d => d.Trim(new Char[] { ' ', '"' })).ToArray();
+
+                    String fname = data[1].Substring(data[1].LastIndexOf("/") + 1);
+                    String Label = fname.Split('.')[0];
+
+                    sd.FileName = fname;
+                    sd.Label = Label;
+                    sd.Type = "Ambience";
+                    sd.Title = Label;
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Sounds, sd);
+
+                    //Now do the command for it
+                    Command sdc = new Command();
+                    sdc.Comm = "Ambience";
+                    sdc.Arg[0] = Label;
+                    sdc.Arg[1] = "TRUE"; //loop
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, sdc);
+                }
+                else if (line.StartsWith("wavestop", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Handle a command to stop the looping wave command
+                    Command ws = new Command();
+
+                    ws.Comm = "StopAmbience";
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, ws);
+                }
+                else if (line.StartsWith("bgm", StringComparison.OrdinalIgnoreCase) && !line.StartsWith("bgmloop", StringComparison.OrdinalIgnoreCase))
+                {
+                    //Handle a BGM
+                    Sound sd = new Sound();
+                    String tline = line;
+                    if (line.StartsWith("bgm\"", StringComparison.OrdinalIgnoreCase))
+                    {
+                        tline = line.Substring(0, 3) + " \"" + line.Substring(4);
+                    }
+                    String[] data = tline.Split(' ').Select(d => d.Trim(new Char[] { ' ', '"' })).ToArray();
+
+                    String fname = data[1].Substring(data[1].LastIndexOf("/") + 1);
+                    String Label = fname.Split('.')[0];
+
+                    sd.FileName = fname;
+                    sd.Label = Label;
+                    sd.Type = "Bgm";
+                    sd.Title = Label;
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Sounds, sd);
+
+                    //Now do the command for it
+                    Command sdc = new Command();
+                    sdc.Comm = "Bgm";
+                    sdc.Arg[0] = sd.Label;
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, sdc);
+                }
+                else if (line.StartsWith("bgmloop", StringComparison.OrdinalIgnoreCase))
+                {
                     //TODO
 
                 }
-                else if(line.StartsWith("`"))
+                else if (line.StartsWith("stop", StringComparison.OrdinalIgnoreCase))
+                { 
+                    //Handle a command to stop all sound
+                    Command ws = new Command();
+
+                    ws.Comm = "StopSound";
+
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, ws);
+
+                }
+                else if (line.StartsWith("`"))
                 {
                     //Handle a text command
                     Command text = new Command();
@@ -89,11 +196,15 @@ namespace NscripterConverter
                     wait.Comm = "Wait";
                     wait.Arg[5] = (Int32.Parse(data[1]) / 1000).ToString(); //converts milliseconds to seconds
 
-                    curr.AddTo(Label.LabelTypes.Commands, wait);
+                    curr.AddTo(NscripterConverter.Label.LabelTypes.Commands, wait);
+                }
+                else if (line.StartsWith("quake", StringComparison.OrdinalIgnoreCase))
+                {
+
                 }
                 else
                 {
-                    //Handle something else that is probably not needed
+                    //Handle something else that is probably not needed but probably will end up being useful
                 }
             }
 
